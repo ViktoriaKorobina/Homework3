5c4ce02a1216efc9534a41b3df986c5813130740
Viktoria <viktoria.korobjina@yandex.ru>
2023-09-09 12:57:48 +0300

    Добавлено решение задачи 21

---
+++ task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21: Напишите программу, которая принимает на
+// вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+
+double xa = WorkWithUser("Введите координату Xa: ");
+double ya = WorkWithUser("Введите координату Ya: ");
+double za = WorkWithUser("Введите координату Za: ");
+double xb = WorkWithUser("Введите координату Xb: ");
+double yb = WorkWithUser("Введите координату Yb: ");
+double zb = WorkWithUser("Введите координату Zb: ");
+double distance = Distance(xa, ya, za, xb, yb, zb);
+Console.WriteLine(distance);
+
+double WorkWithUser(string message)
+{
+    Console.Write(message);
+    double number = double.Parse(Console.ReadLine());
+    return number;
+}
+double Distance(double Xa, double Ya, double Za, double Xb, double Yb, double Zb)
+{
+    double a = Xa - Xb;
+    double b = Ya - Yb;
+    double c = Za - Zb; 
+    return Math.Sqrt(a * a + b * b + c * c);
+}
